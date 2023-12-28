525ba4d2e76496bab20ec6407fc382cbaedc3ee5
jansmailbegovic <jan.smailbegovic@gmail.com>
2023-12-28 00:11:10 -0500

    gh link target

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className="p-2 fixed h-fit w-[100%] left-0 bottom-0 text-center text-black sm:relative sm:bg-slate-200">
             <div>
                 Made by 
-                    {<Link href='https://github.com/jansm04'>
+                    {<Link href='https://github.com/jansm04' target="_blank">
                         <div className="text-indigo-700 inline"> Jan S. </div>
                     </Link>} 
                 in December 2023
