307f138136df4857a7e6efe30a70ea80a6b0885e
jansmailbegovic <jan.smailbegovic@gmail.com>
2024-01-10 02:03:08 -0500

    refactoring

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -10,15 +10,15 @@ import Footer from "@/components/footer";
 import Place from "@/interfaces/place"
 
 // assets
-import lp from "@/assets/loading.json"
-import ep from "@/assets/error.json"
+import ep from "@/objects/error"
 
 const Home = () => {
 
     const [places, setPlaces] = useState<Place[]>([])
+    const [loading, setLoading] = useState<boolean>(false);
     
     const handleQuerySubmit = (city: string, type: string, mood: string) => {
-        setPlaces([lp]) // loading place object
+        setLoading(true);
 
         // fetch array of restaurants from OpenAI API
         const fetchRestaurants = async () => {
@@ -30,25 +30,28 @@ const Home = () => {
             } else {
                 setPlaces([ep]) // error place object
             }
+            setLoading(false);
         }
         fetchRestaurants()
     }
 
     return (
-        <div>
-            <div className="grid grid-cols-3 min-h-screen sm:block">
-                <PlaceForm onSubmit={handleQuerySubmit} />
-                <div className="p-20 bg-slate-200 col-span-2 sm:py-20 sm:px-8 sm:h-[100%] sm:max-h-fit sm:min-h-[50%]">
-                    <h2 className="text-[#3a3c3d] mt-10 mb-[60px] font-bold text-xl sm:mt-0 sm:text-3xl"> Your Top Choices: </h2>
-                    <div className="mb-[12%] sm:mb-12">
-                        {places && places.map((place) => (
-                            <PlaceDetails key={place.id} place={place}/>
-                        ))}
+            <div>
+                <div className="grid grid-cols-3 min-h-screen sm:block">
+                    <PlaceForm onSubmit={handleQuerySubmit} />
+                    <div className="p-20 bg-slate-200 col-span-2 sm:py-20 sm:px-8 sm:h-[100%] sm:max-h-fit sm:min-h-[50%]">
+                        <h2 className="text-[#3a3c3d] mt-10 mb-[60px] font-bold text-xl sm:mt-0 sm:text-3xl"> Your Top Choices: </h2>
+                        <div className="mb-[12%] sm:mb-12">
+                            {loading ? 
+                                <p className="text-gray-800">loading...</p> :
+                                places && places.map((place) => (
+                                    <PlaceDetails key={place.id} place={place}/>
+                                ))}
+                        </div>
                     </div>
                 </div>
-            </div>
-            <Footer />
-        </div>
+                <Footer />
+            </div>        
     )
 }
 
--- src/components/place-details.tsx
+++ src/components/place-details.tsx
@@ -5,36 +5,17 @@ const PlaceDetails = ({ key, place } : {
     key: string, 
     place: Place 
 }) => {
-
-    if (place && place.id === "loading") {
-        return (
-            <div className="text-[#2a2b2c]">
-                <p>{place.displayName.text}</p>
-            </div>
-        )
-    }
-
-    if (place && place.id === "error") {
-        return (
-            <div className="text-[#2a2b2c]">
-                <p>{place.displayName.text}</p>
-            </div>
-        )
-    }
- 
-    if (place && place.websiteUri && place.rating && place.formattedAddress) {
-        return (
-            <div className="bg-[#1d1d1d] m-4 p-2.5 text-center text-[#c2c2bd] rounded-lg">
-                <Link className="italic font-bold text-[#ddd3a1] m-0.5 hover:underline sm:text-lg" href={place.websiteUri} target="_blank">{place.displayName.text}</Link>
-                <p className='m-0.5'>{place.rating}⭐️</p>
-                <p className='m-0.5'>📍{place.formattedAddress}</p>
-            </div> 
-        )
-    } else {
-        console.log("Null place object.")
-        return <></>
-    }
-    
+    return (
+        (place && place.id === "error") ? 
+            <p className="text-gray-800">{place.displayName.text}</p> :
+            (place && place.websiteUri && place.rating && place.formattedAddress) ?
+                <div className="bg-[#1d1d1d] m-4 p-2.5 text-center text-[#c2c2bd] rounded-lg">
+                    <Link className="italic font-bold text-[#ddd3a1] m-0.5 hover:underline sm:text-lg" href={place.websiteUri} target="_blank">{place.displayName.text}</Link>
+                    <p className='m-0.5'>{place.rating}⭐️</p>
+                    <p className='m-0.5'>📍{place.formattedAddress}</p>
+                </div> :
+                <></>
+    )
 }
 
 export default PlaceDetails
