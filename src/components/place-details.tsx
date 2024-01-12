1a3261172678e769bf0e1832c13de10806e8bd4d
jansmailbegovic <jan.smailbegovic@gmail.com>
2024-01-11 21:58:08 -0500

    added new tab icon

--- src/components/place-details.tsx
+++ src/components/place-details.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import Place from "@/interfaces/place"
+import { ImNewTab } from "react-icons/im";
 
 const PlaceDetails = ({ key, place } : { 
     key: string, 
@@ -10,7 +11,9 @@ const PlaceDetails = ({ key, place } : {
             <p className="text-gray-800">{place.displayName.text}</p> :
             (place && place.websiteUri && place.rating && place.formattedAddress) ?
                 <div className="bg-[#1d1d1d] m-4 p-2.5 text-center text-[#c2c2bd] rounded-lg">
-                    <Link className="italic font-bold text-[#ddd3a1] m-0.5 hover:underline sm:text-lg" href={place.websiteUri} target="_blank">{place.displayName.text}</Link>
+                    <Link className="italic font-bold text-[#ddd3a1] m-0.5 hover:underline sm:text-lg inline-flex" href={place.websiteUri} target="_blank">
+                        {place.displayName.text}&nbsp;<ImNewTab className="mt-[4px]"/>
+                    </Link>
                     <p className='m-0.5'>{place.rating}⭐️</p>
                     <p className='m-0.5'>📍{place.formattedAddress}</p>
                 </div> :
