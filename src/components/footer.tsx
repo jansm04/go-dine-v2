54c7e910c8dab2f64b1570e8b967787fedad8d3d
jansmailbegovic <jan.smailbegovic@gmail.com>
2024-03-29 15:38:22 -0400

    updated footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,13 +2,12 @@ import Link from "next/link"
 
 const Footer = () => {
     return (
-        <footer className="p-2 fixed h-fit w-[100%] left-0 bottom-0 text-center text-black sm:relative sm:bg-slate-200">
+        <footer className="p-2 fixed h-fit w-[100%] left-0 bottom-0 text-center text-[14px] text-gray-600 sm:relative sm:bg-slate-200">
             <div>
-                Made by 
-                    {<Link href='https://github.com/jansm04' target="_blank">
-                        <div className="text-indigo-700 inline"> @jansm04 </div>
-                    </Link>} 
-                in 2023
+                Made by @jansm04 in 2023&nbsp;&nbsp;•&nbsp;&nbsp; 
+                {<Link href='https://github.com/jansm04/recursion-visualizer' target="_blank">
+                    <div className="text-sky-700 inline">Github</div>
+                </Link>}
             </div>
         </footer>
     )
